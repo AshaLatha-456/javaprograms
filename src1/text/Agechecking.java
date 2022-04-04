7c791562453cb2829985cba70f25212b571168e5
AshaLatha-456 <102552899+AshaLatha-456@users.noreply.github.com>
2022-04-04 17:57:27 -0500

    Update Agechecking.java

--- src1/text/Agechecking.java
+++ src1/text/Agechecking.java
@@ -5,7 +5,7 @@ public class Agechecking {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
      Ageforvoting age1=new Ageforvoting();
-     boolean result=age1.Eligible(18);
+     boolean result=age1.Eligible(8);
      System.out.println(result);
 	}
 
